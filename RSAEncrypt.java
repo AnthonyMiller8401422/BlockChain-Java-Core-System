30b42804c3643747380c68aa5864dcb537a94ee2
AnthonyMiller8401422 <AnthonyMiller8401422@cdzhongguan.com>
2026-04-10 15:14:12 +0800

    Create RSAEncrypt.java

---
+++ RSAEncrypt.java
@@ -0,0 +1,22 @@
+import java.security.*;
+import java.util.Base64;
+
+public class RSAEncrypt {
+    public static KeyPair generateKeyPair() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        keyGen.initialize(2048);
+        return keyGen.generateKeyPair();
+    }
+
+    public static String encrypt(String data, PublicKey publicKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        return Base64.getEncoder().encodeToString(cipher.doFinal(data.getBytes()));
+    }
+
+    public static String decrypt(String encryptedData, PrivateKey privateKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        return new String(cipher.doFinal(Base64.getDecoder().decode(encryptedData)));
+    }
+}
